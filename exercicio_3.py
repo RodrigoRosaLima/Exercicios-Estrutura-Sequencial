2439c8c07962b598fd9502dd3aa171d02bbc6b44
RodrigoRosaLima <rodrigo.rosa.lima@hotmail.com>
2021-10-01 15:16:51 -0300

    Create exercicio_3.py

---
+++ exercicio_3.py
@@ -0,0 +1,7 @@
+# 3 - Faça um Programa que peça dois números e imprima a soma.
+
+num_1 = float(input('Digite um número: '))
+num_2 = float(input('Digite outro número: '))
+soma = num_1 + num_2
+
+print(f'A soma dos número é {soma:.2f}.')
