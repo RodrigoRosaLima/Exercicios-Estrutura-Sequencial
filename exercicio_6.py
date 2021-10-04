5b77184ef5098c0802d219cb40adde91ecd4bdf4
RodrigoRosaLima <rodrigo.rosa.lima@hotmail.com>
2021-10-04 09:38:58 -0300

    Create exercicio_6.py

---
+++ exercicio_6.py
@@ -0,0 +1,7 @@
+# 6 - Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+raio = int(input('Qual o valor do raio que deseja saber a área total? --> '))
+resultado = 3.14 * raio ** 2
+
+print(f'O valor total da área é {resultado}')
+
